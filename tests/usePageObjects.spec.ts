9802e9a45d677ce1ddebf7cd9d55f824fc40bc59
Steven <stevenericborkowski@gmail.com>
2025-07-03 18:08:12 -0400

    more pom

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,12 +1,16 @@
 import { test, expect } from "@playwright/test"; // import playwright
-import { NavigationPage } from '../page-Objects/navigationPage' //import navigationPage.ts
+import { NavigationPage } from "../page-Objects/navigationPage"; //import navigationPage.ts
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
 });
 
-test('navigate to form page', async ({page}) => {
-  const navigateTo = new NavigationPage (page)
-  await navigateTo.formLayoutsPage()
-    
-})
+test("navigate to form page", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  await navigateTo.formLayoutsPage();
+  await navigateTo.datePickerPage();
+  await navigateTo.smartTablePage;
+  await navigateTo.toastrPage();
+  await navigateTo.toolTipPage();
+  
+});
