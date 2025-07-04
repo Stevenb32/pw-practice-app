625964ce0d61f5c05cb4c4214d4d09c57eff03ca
Steven Borkowski <steveneirborkowski@gmail.com>
2025-07-04 07:34:02 -0400

    added locators to object but not using since teacher doesnt like this way

--- page-Objects/navigationPage.ts
+++ page-Objects/navigationPage.ts
@@ -3,43 +3,48 @@ import { Locator, Page } from "@playwright/test";
 
 export class NavigationPage {
   readonly page: Page;
-  readonly formLayOutMenuItem: Locator
-  readonly datePickerMenuItem: Locator
-  readonly smartTableMenuItem: Locator
-  readonly toastrMenuItem: Locator
-  readonly toolTipMenuItem: Locator
+  // readonly formLayOutMenuItem: Locator
+  // readonly datePickerMenuItem: Locator
+  // readonly smartTableMenuItem: Locator
+  // readonly toastrMenuItem: Locator
+  // readonly toolTipMenuItem: Locator
 
   constructor(page: Page) {
     this.page = page
-    this.formLayOutMenuItem = page.getByText("Form Layouts")
-    this.datePickerMenuItem = page.getByText("Datepicker")
-    this.smartTableMenuItem = page.getByText("Smart Table")
-    this.toastrMenuItem = page.getByText("Toastr")
-    this.toolTipMenuItem = page.getByText("Tooltip")
+    // this.formLayOutMenuItem = page.getByText("Form Layouts")
+    // this.datePickerMenuItem = page.getByText("Datepicker")
+    // this.smartTableMenuItem = page.getByText("Smart Table")
+    // this.toastrMenuItem = page.getByText("Toastr")
+    // this.toolTipMenuItem = page.getByText("Tooltip")
   }
   async formLayoutsPage() {
     await this.selectGroupMenuItem('Forms')
-    await this.formLayOutMenuItem.click()
+    // await this.formLayOutMenuItem.click()
+    await this.page.getByText("Form Layouts").click()
   }
 
   async datePickerPage(){
     await this.selectGroupMenuItem('Forms')
-    await this.datePickerMenuItem.click()
+    // await this.datePickerMenuItem.click()
+    await this.page.getByText("Datepicker").click()
   }
 
   async smartTablePage(){
     await this.selectGroupMenuItem('Tables & Data')    
-    await this.smartTableMenuItem.click()
+    // await this.smartTableMenuItem.click()
+    await this.page.getByText("Smart Table").click()
   }
 
   async toastrPage(){
     await this.selectGroupMenuItem('Modal & Overlays')      
-    await this.toastrMenuItem.click()
+    // await this.toastrMenuItem.click()
+    await this.page.getByText("Toastr").click()
   }
 
   async toolTipPage(){
     await this.selectGroupMenuItem('Modal & Overlays')      
-    await this.toolTipMenuItem.click()
+    // await this.toolTipMenuItem.click()
+    await this.page.getByText("Tooltip").click()
   }
 
   private async selectGroupMenuItem(groupItemTitle: string){
